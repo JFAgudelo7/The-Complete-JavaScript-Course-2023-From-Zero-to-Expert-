071704d868d28b37b4f8547910a9962fa73a5faf
JF Agudelo <jagudelo9@gmail.com>
2023-09-19 20:33:10 -0500

    section 13 - Adding Scrolling

--- 13-Advanced-DOM-Events/script.js
+++ 13-Advanced-DOM-Events/script.js
@@ -30,3 +30,61 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+///////////////////////////////////////
+// Creating Elements
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `We use cookies fot improve functionality and analytics, <button class="btn btn--close-cookie">Got it!</button>`;
+
+header.append(message);
+
+///////////////////////////////////////
+// Delete Elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+});
+
+///////////////////////////////////////
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.widht = '120%';
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+//document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+logo.alt = "Beautiful minimalist logo";
+
+///////////////////////////////////////
+// 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener('click', function(e){
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // Scrolling
+  //window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY); --> Old school 1
+
+  // Old school 2
+  /*window.scrollTo({
+    left: s1coords.left + window.scrollX, 
+    top: s1coords.top + window.scrollY,
+    behavior: 'smooth'
+  });*/
+
+  // SCROLLING----------------------------------------------
+  section1.scrollIntoView({behavior: 'smooth'});
+  // ----------------------------------------------
+
+
+
+})
+
