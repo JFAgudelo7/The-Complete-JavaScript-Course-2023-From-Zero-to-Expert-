60b507e43492aeaa5222f0be139700fa0cf95512
JF Agudelo <jagudelo9@gmail.com>
2023-09-21 19:53:40 -0500

    Passgin arguments to event handlers

--- 13-Advanced-DOM-Events/script.js
+++ 13-Advanced-DOM-Events/script.js
@@ -1,42 +1,45 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach( btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
 // --------------------------------------------------------------------------------------------
 // Scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector("#section--1");
 
-btnScrollTo.addEventListener('click', function(e){
+btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
 
@@ -51,9 +54,8 @@ btnScrollTo.addEventListener('click', function(e){
   });*/
 
   // SCROLLING----------------------------------------------
-  section1.scrollIntoView({behavior: 'smooth'});
+  section1.scrollIntoView({ behavior: "smooth" });
   // ----------------------------------------------
-
 });
 
 // --------------------------------------------------------------------------------------------
@@ -76,76 +78,105 @@ Page Navigation with Event Delegation
 1. Add Event listener to common parent element -> nav__links is the container of the nav links
 2. Determine what element originated the event
 */
-document.querySelector('.nav__links').addEventListener('click', function(e){
+document.querySelector(".nav__links").addEventListener("click", function (e) {
   //console.log(e.target);
   e.preventDefault();
 
   //Matching strategy
-  if(e.target.classList.contains('nav__link')){    
-    const id = e.target.getAttribute('href');
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
     console.log(id);
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
-})
-
+});
 
 // --------------------------------------------------------------------------------------------
-// Tabbed Component 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+// Tabbed Component
 
-tabsContainer.addEventListener('click', function(e){
-  const clicked = e.target.closest('.operations__tab');
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
   console.log(clicked);
 
   //Guard clause
-  if(!clicked) return;
+  if (!clicked) return;
 
   //Remove active classes
-  tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
 
   //Active tab
-  clicked.classList.add('operations__tab--active');
+  clicked.classList.add("operations__tab--active");
 
   //Activate content area
   console.log(clicked.dataset.tab);
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
-})
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+// --------------------------------------------------------------------------------------------
+// Menu Fade Animation
+
+const handleHover = function(e, opacity){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el != link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// We need to pass a function, not the result of calling a function, to the addEventListener, for that we need to use the following structure, or the bind structure
+/*nav.addEventListener('mouseover', function(e){
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', function(e){
+  handleHover(e, 1);
+});*/
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 //--------------------------------------------------------------------------------------------
 //----------------------------------------------SECTIONS
 
-  // ----------------------------------------------
+// ----------------------------------------------
 // Creating Elements
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+const message = document.createElement("div");
+message.classList.add("cookie-message");
 message.innerHTML = `We use cookies fot improve functionality and analytics, <button class="btn btn--close-cookie">Got it!</button>`;
 
 header.append(message);
 
-  // ----------------------------------------------
+// ----------------------------------------------
 // Delete Elements
-document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-  message.remove();
-});
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
 
-  // ----------------------------------------------
+// ----------------------------------------------
 // Styles
-message.style.backgroundColor = '#37383d';
-message.style.widht = '120%';
+message.style.backgroundColor = "#37383d";
+message.style.widht = "120%";
 
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
 
 //document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 // Attributes
-const logo = document.querySelector('.nav__logo');
+const logo = document.querySelector(".nav__logo");
 logo.alt = "Beautiful minimalist logo";
-
 
 // ----------------------------------------------
 // Event Propagation
