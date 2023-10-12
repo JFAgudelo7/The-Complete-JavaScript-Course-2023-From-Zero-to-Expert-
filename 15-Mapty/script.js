1a5598172bd58ebd1edf9ca4a07b7317a5f8a33d
JF Agudelo <jagudelo9@gmail.com>
2023-10-12 18:06:22 -0500

    Adding Markers

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -19,7 +19,7 @@ if (navigator.geolocation) {
       console.log(position);
       console.log(latitude + " " + longitude);
 
-    const coords = [latitude, longitude];
+      const coords = [latitude, longitude];
 
       const map = L.map("map").setView(coords, 13);
 
@@ -28,10 +28,17 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        })).setPopupContent('Workout').openPopup();
+      });
     },
     function () {
       alert("Could not get your position");
